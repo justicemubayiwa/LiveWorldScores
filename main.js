2b378668c1d9e3d97c04d5e32003b0e0ff414879
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-17 19:53:50 -0600

    det all DOM elements i will manipulateinto variables

---
+++ main.js
@@ -0,0 +1,10 @@
+var elapsedTime = document.querySelector("#elapsed");
+var homeTeamImage = document.querySelector("#homeLogo");
+var homeTeamName = document.querySelector("#homeName");
+var awayTeamImage = document.querySelector("#awayLogo");
+var awayTeamName = document.querySelector("#awayName");
+var lastMatchGoal = document.querySelector("#goals");
+var matchTable = document.querySelector("#matchTable");
+
+
+
